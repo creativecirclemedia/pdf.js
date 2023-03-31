fa241001af3ecb063b5bb73d497565cee8502d4a
Tim Benson <tim@creativecirclemedia.com>
2023-03-31 11:18:13 -0400

    Fix boolean

--- web/app.js
+++ web/app.js
@@ -3313,7 +3313,6 @@ function webViewerAnnotationEditorStatesChanged(data) {
 }
 
 function unsupportedIosPresent() {
-  return true;
   if (/iP(hone|od|ad)/.test(navigator.platform)) {
     // supports iOS 2.0 and later: <http://bit.ly/TJjs1V>
     var v = (navigator.appVersion).match(/OS (\d+)_(\d+)_?(\d+)?/);
