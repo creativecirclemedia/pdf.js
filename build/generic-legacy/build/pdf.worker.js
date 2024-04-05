28525da2e875d5e6167fc0cb1b797c3cf9667b00
Tim Benson <tim@creativecirclemedia.com>
2024-04-05 09:51:53 -0400

    Update build

--- build/generic-legacy/build/pdf.worker.js
+++ build/generic-legacy/build/pdf.worker.js
@@ -101,7 +101,7 @@ class WorkerMessageHandler {
       docId,
       apiVersion
     } = docParams;
-    const workerVersion = '3.5.134';
+    const workerVersion = '3.5.136';
     if (apiVersion !== workerVersion) {
       throw new Error(`The API version "${apiVersion}" does not match ` + `the Worker version "${workerVersion}".`);
     }
@@ -68766,8 +68766,8 @@ Object.defineProperty(exports, "WorkerMessageHandler", ({
   }
 }));
 var _worker = __w_pdfjs_require__(1);
-const pdfjsVersion = '3.5.134';
-const pdfjsBuild = 'eb952be46';
+const pdfjsVersion = '3.5.136';
+const pdfjsBuild = '28ef9317f';
 })();
 
 /******/ 	return __webpack_exports__;
